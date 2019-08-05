519e1f45c4006b910bec747f03c46051f99b29ee
Gregor Taylor (student) <g.taylor.3@research.gla.ac.uk>
2019-08-05 11:46:17 +0100

    Updated

--- snspd_design.py
+++ snspd_design.py
@@ -50,19 +50,21 @@ def generate_cross():
 def generate_grill():
     pass
 
-def generate_taper(start_width, end_width, length, footprint, thickness=0.5, start_point=(0,0), orientation='l'): #footprint in form (x,y) 
+def generate_taper(start_width, end_width, length, footprint, thickness=0.5, start_point=(0,0), orientation='r', corner_correction=3): #footprint in form (x,y) 
     taper=gdspy.Path(thickness, start_point, number_of_paths=2, distance=(start_width+thickness))
     taper.turn((start_width),'l') #first turn to go out of the nanowire
     #calculate the length of each straight section
-    number_lines=length/footprint[0]
-    taper_per_line=(end_width-start_width)/number_lines
-    current_width=start_width
+    number_lines=length/footprint[0]-corner_correction #corner_correction tunes the length to account for the lengths in the corners.
+    taper_per_line=(end_width-start_width)/number_lines #how much to taper on each straight
+    current_width=start_width 
     line_length=footprint[0]
     #First and last straight only goes half way.
+    #First straight
     current_width+=(taper_per_line/2)
     taper.segment((line_length/2), '+y', final_distance=(current_width))
     taper.turn((current_width),'rr')
-    neg=True
+    neg=True #Keeps track of which end is the corner
+    #Main body of lines
     for i in range(int(round(number_lines-1))):
         current_width+=taper_per_line
         if neg==True:
@@ -73,15 +75,19 @@ def generate_taper(start_width, end_width, length, footprint, thickness=0.5, sta
             taper.segment((line_length), '+y', final_distance=(current_width))
             taper.turn((current_width),'rr')
             neg=True
-    #and add end half
+    #Last straight
     current_width+=(taper_per_line/2)
+    #Sets the end of the taper to be exactly as requested.
+    if round(current_width)!=end_width:
+        print(f'End width is {current_width}, setting last line taper to end in {end_width} instead')
+        current_width=end_width
     if neg==True:
         taper.segment((line_length/2), '-y', final_distance=(current_width))
         taper.turn((current_width),'l')
     else:
         taper.segment((line_length/2), '+y', final_distance=(current_width))
         taper.turn((current_width),'r')
-
+    #Allows to rotate the taper so it can go in any direction
     if orientation!='r':
         if orientation=='d':
             taper.rotate((3*np.pi)/2)
@@ -89,10 +95,17 @@ def generate_taper(start_width, end_width, length, footprint, thickness=0.5, sta
             taper.rotate((np.pi)/2)
         if orientation=='l':
             taper.rotate((np.pi))
+    #Next section to remove lines to account for the lengths of the corners. Works by tuning the corner correction parameter until the line.length is within
+    #acceptable limits.
+    #Below could result in infinite loop... Needs fixed
+    if taper.length > (length+1000): #accept within 1um either way.
+        corner_correction+=1
+        taper = generate_taper(start_width, end_width, length, footprint,corner_correction=corner_correction)
+    elif taper.length < (length-1000):
+        corner_correction-=1
+        taper = generate_taper(start_width, end_width, length, footprint,corner_correction=corner_correction)
 
-
-    print(current_width, taper_per_line, number_lines)
-
+    print(f'Taper length:{taper.length}')
     return taper
 
 
