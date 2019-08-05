aece50d9d24191ed4244862237e5a15ff2975bfe
Gregor Taylor (student) <g.taylor.3@research.gla.ac.uk>
2019-08-05 15:13:01 +0100

    Updated geometries

--- snspd_design.py
+++ snspd_design.py
@@ -6,8 +6,7 @@ nw_cell=gdspy.Cell('Nanowire')
 def generate_nw(line, gap, area, prox_buffer=0.5, end=0.5, start_point=(0,0), round_ends=True):
     points=[start_point]
     number_of_lines = (area/((2*gap)+(2*line)))
-    print(number_of_lines)
-    current_x, current_y = 0,0
+    current_x, current_y = start_point[0],start_point[1]
     #draw buffer side first
     current_y+=(area+(2*prox_buffer)+line)
     points.append((current_x, current_y))
@@ -44,13 +43,70 @@ def generate_nw(line, gap, area, prox_buffer=0.5, end=0.5, start_point=(0,0), ro
 
     return poly, poly2
 
-def generate_cross():
-    pass
+def generate_cross(size,thickness, start_point=(0,0)):
+    points=[(start_point[0]+(size/2)-(thickness/2), start_point[1])]
+    current_x, current_y=points[0][0], points[0][1]
+    long_side=(size/2)-(thickness/2)
+    current_y+=long_side
+    points.append((current_x, current_y))
+    current_x-=long_side
+    points.append((current_x, current_y))
+    current_y+=thickness
+    points.append((current_x, current_y))
+    current_x+=long_side
+    points.append((current_x, current_y))
+    current_y+=long_side
+    points.append((current_x, current_y))
+    current_x+=thickness
+    points.append((current_x, current_y))
+    current_y-=long_side
+    points.append((current_x, current_y))
+    current_x+=long_side
+    points.append((current_x, current_y))
+    current_y-=thickness
+    points.append((current_x, current_y))
+    current_x-=long_side
+    points.append((current_x, current_y))
+    current_y-=long_side
+    points.append((current_x, current_y))
+    current_x-=thickness
+    points.append((current_x, current_y))
+    cross=gdspy.Polygon(points)
+    return cross
 
-def generate_grill():
-    pass
+def generate_grill(size, thickness, spacing=None,start_point=(0,0)):
+    set_of_poly=[]
+    points=[start_point]
+    #If spacing not equal to thickness then change from None
+    if spacing==None:
+        spacing=thickness
+    #first one
+    current_x, current_y=points[0][0], points[0][1]
+    current_y+=size
+    points.append((current_x, current_y))
+    current_x+=thickness
+    points.append((current_x, current_y))
+    current_y-=size
+    points.append((current_x, current_y))
+    set_of_poly.append(points)
+    for i in range(int(size/(thickness+spacing))):
+        points=[]
+        current_x+=spacing
+        points.append((current_x, current_y))
+        current_y+=size
+        points.append((current_x, current_y))
+        current_x+=thickness
+        points.append((current_x, current_y))
+        current_y-=size
+        points.append((current_x, current_y))
+        set_of_poly.append(points)
 
-def generate_taper(start_width, end_width, length, footprint, thickness=0.5, start_point=(0,0), orientation='r', corner_correction=3, rec_flag=False): #footprint in form (x,y) 
+    grill=gdspy.PolygonSet(set_of_poly)
+    return grill
+  
+
+def generate_taper(start_width, end_width, length, footprint, thickness=0.5, start_point=(0,0), orientation='r', corner_correction=3, rec_flag=False): 
+    #footprint in form (x,y) 
     taper=gdspy.Path(thickness, start_point, number_of_paths=2, distance=(start_width+thickness))
     taper.turn((start_width),'l') #first turn to go out of the nanowire
     #calculate the length of each straight section
@@ -110,13 +166,31 @@ def generate_taper(start_width, end_width, length, footprint, thickness=0.5, sta
     print(f'Taper length:{taper.length}')
     return taper
 
+def generate_cpw(start_width, end_width, gap, length, orientation='u',start_point=(0,0), contact_pad=True):
+    cpw=gdspy.Path(gap, start_point, number_of_paths=2, distance=(start_width+gap))
+    #square section to start#
+    cpw.segment((start_width), '+y')
+    #taper it up
+    cpw.segment(length, '+y', final_distance=end_width)
+    if contact_pad==True:
+        cpw.segment((end_width+(2*gap)), '+y')
+    if orientation!='u':
+        if orientation=='r':
+            cpw.rotate((3*np.pi)/2)
+        if orientation=='l':
+            cpw.rotate((np.pi)/2)
+        if orientation=='d':
+            cpw.rotate((np.pi))
+    return cpw
 
-
-
+    
 
 #nw1_1, nw1_2=generate_nw(0.05,0.1, 10.2)
 
 #nw_cell.add([nw1_1,nw1_2])
-taper=generate_taper(0.5, 135, 77900, (2300,1100))
-nw_cell.add(taper)
+#taper=generate_taper(0.5, 135, 77900, (2300,1100))
+#cpw=generate_cpw(0.5, 200, 1,500)
+#cross=generate_cross(100,10)
+grill=generate_grill(10, 1)
+nw_cell.add(grill)
 gdspy.LayoutViewer()
